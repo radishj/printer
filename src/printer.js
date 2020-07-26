e51dc722ad84c6b163ca453a02a0d3104d0d74ee
radishj@gmail.com <radishj@gmail.com>
2020-07-26 03:30:13 -0700

    add DIRECT_PAY

--- src/printer.js
+++ src/printer.js
@@ -1,6 +1,6 @@
 const ThermalPrinter = require("./node-thermal-printer").printer;
 const Types = require("./node-thermal-printer").types;
-const OrderType = require("tasti-cloud-lib/lib/order/index")
+const OrderType = require("@dxj/tasti-lib/lib/order").OrderType;
 let db = null //require('./fb');
 
 function sendCommand(printer, command){
@@ -182,6 +182,8 @@ async function print (data) {
 function printOrder(data){
 try{
     const order = data.order;
+    console.log('order:', JSON.stringify(order, null, '  '))
+    //return
     var pData=[];
     //console.log('order:', JSON.stringify(order,null,"   "));
     pData.push(["align","center"]);
@@ -193,116 +195,119 @@ try{
     pData.push(["align","left"]);
     pData.push(["println","Order#: "+order.readableOrderNum]);
     pData.push(["println","Customer Info:"]);
-    pData.push(["printlr","Name:"+order.userFullName, "Phone:"+order.phoneNumber]);
-    pData.push(["println","Placed: "+order.orderTime]);
-    //if(o.OrderType[order.orderType] === "DELIVERY"){
-    //    pData.push(["println","Address: "+order.customerAddress]);
-    //}
-    //else { //if(order.orderType.toLowerCase()==="pickup"){
-        pData.push(["align","center"]);
-        pData.push(["println","Pickup Time"]);
-        var pickupTime = order.orderTime;
-        //pickupTime.setMinutes(pickupTime.getMinutes()+20);
-        //var dateStr = pickupTime.toLocaleDateString('en-US');
-        //dateStr = dateStr.slice(0,dateStr.length-5);
-        //pData.push(["println",dateStr+" "+pickupTime.toLocaleTimeString('en-US')]);
-        pData.push(["println",pickupTime]); //.toLocaleTimeString('en-US')]);
-    //}
-    pData.push(["align","left"]);
-    pData.push(["println","Order Details:"]);
-    var total=0;
-    //console.log('aaaaaaaaaaaaa',order)
-    //pData.push(["align","center"]);
-    order.ticket.cartSnapshot.products.forEach(product => {
-        mainItemPrice = product.productTotalPrice / product.productCount
-        let options = product.options?product.options:[]
-        options.forEach(option => {
-            mainItemPrice -= option.additionalOptionPrice
-        })
-        var optionsTotal=0;
-        var optionsArr="";
-        if(product.productCount==1)
-            optionsArr=[[['Main item', product.productName, mainItemPrice.toFixed(2)]]];
-        else
-            optionsArr=[[['Main item', product.productName, mainItemPrice.toFixed(2)+' x '+product.productCount]]];
-        var optionCategoryName="        ";
-        var categoryCount = 0;
-        options.sort(function(a, b){
-            if(a.optionCategoryName){
-                if(b.optionCategoryName){
-                    return a.optionCategoryName > b.optionCategoryName
+    pData.push(["printlr","Name:"+order.userFullName, "Phone:"+order.userPhone]);
+    pData.push(["println","Placed: "+new Date(order.orderTime).toLocaleTimeString('en-US')]);
+    console.log('order.orderType != OrderType.DIRECT_PAY', order.orderType != OrderType.DIRECT_PAY, order.orderType, OrderType.DIRECT_PAY)
+    if(order.orderType != OrderType.DIRECT_PAY){
+        //if(o.OrderType[order.orderType] === "DELIVERY"){
+        //    pData.push(["println","Address: "+order.customerAddress]);
+        //}
+        //else { //if(order.orderType.toLowerCase()==="pickup"){
+            pData.push(["align","center"]);
+            pData.push(["println","Pickup Time"]);
+            var pickupTime = order.orderTime;
+            //pickupTime.setMinutes(pickupTime.getMinutes()+20);
+            //var dateStr = pickupTime.toLocaleDateString('en-US');
+            //dateStr = dateStr.slice(0,dateStr.length-5);
+            //pData.push(["println",dateStr+" "+pickupTime.toLocaleTimeString('en-US')]);
+            pData.push(["println",new Date(pickupTime).toLocaleTimeString('en-US')]);
+        //}
+        pData.push(["align","left"]);
+        pData.push(["println","Order Details:"]);
+        var total=0;
+        //console.log('aaaaaaaaaaaaa',order)
+        //pData.push(["align","center"]);
+        order.ticket.cartSnapshot.products.forEach(product => {
+            mainItemPrice = product.productTotalPrice / product.productCount
+            let options = product.options?product.options:[]
+            options.forEach(option => {
+                mainItemPrice -= option.additionalOptionPrice
+            })
+            var optionsTotal=0;
+            var optionsArr="";
+            if(product.productCount==1)
+                optionsArr=[[['Main item', product.productName, mainItemPrice.toFixed(2)]]];
+            else
+                optionsArr=[[['Main item', product.productName, mainItemPrice.toFixed(2)+' x '+product.productCount]]];
+            var optionCategoryName="        ";
+            var categoryCount = 0;
+            options.sort(function(a, b){
+                if(a.optionCategoryName){
+                    if(b.optionCategoryName){
+                        return a.optionCategoryName > b.optionCategoryName
+                    }
+                    else{
+                        return 1
+                    }
+                } 
+                else{
+                    return -1
+                }
+            });
+            for(var i = 0; i < options.length; i++) {
+                const option = options[i]
+                if(optionCategoryName !== option.optionCategoryName){
+                    if(option.optionPortions == undefined || option.optionPortions == 1)
+                        optionsArr.push([[
+                            option.optionCategoryName?option.optionCategoryName:'        +',
+                            option.subOptionShortName?option.optionName + '('+option.subOptionShortName+')':option.optionName, 
+                            option.additionalOptionPrice?option.additionalOptionPrice.toFixed(2):'0'
+                        ]]);
+                    else
+                        optionsArr.push([[
+                            option.optionCategoryName?option.optionCategoryName:'        +',
+                            option.subOptionShortName?option.optionName + '('+option.subOptionShortName+')':option.optionName, 
+                            option.additionalOptionPrice?option.additionalOptionPrice.toFixed(2):'0'+' x '+option.optionPortions
+                        ]]);
+                    optionCategoryName=option.optionCategoryName;
+                    categoryCount++;
                 }
                 else{
-                    return 1
+                    if(option.optionPortions==1){//console.log("option.name, option.price:",option.name, option.price);
+                        optionsArr[categoryCount].push(['',option.subOptionShortName?option.optionName + '('+option.subOptionShortName+')':option.optionName, option.additionalOptionPrice?option.additionalOptionPrice.toFixed(2):'0'])}
+                    else
+                        optionsArr[categoryCount].push(['',option.subOptionShortName?option.optionName + '('+option.subOptionShortName+')':option.optionName, option.additionalOptionPrice?option.additionalOptionPrice.toFixed(2):'0'+' x '+option.optionPortions])
                 }
-            } 
-            else{
-                return -1
+            };
+            if(product.productNotes)
+                optionsArr=[[['Dish note', product.productNotes.replace('\n',' '), '']]];
+        
+            pData.push(["set font big"]);
+            pData.push(["printlr",product.productName,product.productTotalPrice?product.productTotalPrice.toFixed(2):'0']);
+            pData.push(["set font small"]);
+            var mainItem = [""]
+            //console.log('bbbbbbbbbbbbbb',optionsArr,'cccccccccccccccc');
+            pData.push(["set width",60]);
+            var leadingSpace='   ';
+            for(var i=0;i<optionsArr.length;i++){
+                pData.push(["table3c", 
+                    ['LEFT','LEFT','RIGHT'],
+                    [23,23,10],
+                    optionsArr[i],
+                    '   ',
+                ]); 
             }
+            pData.push(["set width",48]);  
         });
-        for(var i = 0; i < options.length; i++) {
-            const option = options[i]
-            if(optionCategoryName !== option.optionCategoryName){
-                if(option.optionPortions == undefined || option.optionPortions == 1)
-                    optionsArr.push([[
-                        option.optionCategoryName?option.optionCategoryName:'        +',
-                        option.subOptionShortName?option.optionName + '('+option.subOptionShortName+')':option.optionName, 
-                        option.additionalOptionPrice?option.additionalOptionPrice.toFixed(2):'0'
-                    ]]);
-                else
-                    optionsArr.push([[
-                        option.optionCategoryName?option.optionCategoryName:'        +',
-                        option.subOptionShortName?option.optionName + '('+option.subOptionShortName+')':option.optionName, 
-                        option.additionalOptionPrice?option.additionalOptionPrice.toFixed(2):'0'+' x '+option.optionPortions
-                    ]]);
-                optionCategoryName=option.optionCategoryName;
-                categoryCount++;
-            }
-            else{
-                if(option.optionPortions==1){//console.log("option.name, option.price:",option.name, option.price);
-                    optionsArr[categoryCount].push(['',option.subOptionShortName?option.optionName + '('+option.subOptionShortName+')':option.optionName, option.additionalOptionPrice?option.additionalOptionPrice.toFixed(2):'0'])}
-                else
-                    optionsArr[categoryCount].push(['',option.subOptionShortName?option.optionName + '('+option.subOptionShortName+')':option.optionName, option.additionalOptionPrice?option.additionalOptionPrice.toFixed(2):'0'+' x '+option.optionPortions])
-            }
-        };
-        if(product.productNotes)
-            optionsArr=[[['Dish note', product.productNotes.replace('\n',' '), '']]];
-    
         pData.push(["set font big"]);
-        pData.push(["printlr",product.productName,product.productTotalPrice?product.productTotalPrice.toFixed(2):'0']);
-        pData.push(["set font small"]);
-        var mainItem = [""]
-        //console.log('bbbbbbbbbbbbbb',optionsArr,'cccccccccccccccc');
-        pData.push(["set width",60]);
-        var leadingSpace='   ';
-        for(var i=0;i<optionsArr.length;i++){
-            pData.push(["table3c", 
-                ['LEFT','LEFT','RIGHT'],
-                [23,23,10],
-                optionsArr[i],
-                '   ',
-            ]); 
+        pData.push(["align","LEFT"]);
+        pData.push(["println","Notes:"]);
+        var note=order.ticket.cartSnapshot.orderNotes;
+        var len = parseInt(note.length/48);
+        var nodes=[];
+        for(var i=0; i<len; i++)
+        {
+            if(len>8){
+                note="";
+                break;
+            }
+            pData.push(["println",note.subStr(0,48)]);
+            node = note.slice(48);
         }
-        pData.push(["set width",48]);  
-    });
-    pData.push(["set font big"]);
-    pData.push(["align","LEFT"]);
-    pData.push(["println","Notes:"]);
-    var note=order.ticket.cartSnapshot.orderNotes;
-    var len = parseInt(note.length/48);
-    var nodes=[];
-    for(var i=0; i<len; i++)
-    {
-        if(len>8){
-            note="";
-            break;
-        }
-        pData.push(["println",note.subStr(0,48)]);
-        node = note.slice(48);
+        if(note!=='')
+            pData.push(["println",note]);
+        pData.push(["draw line"]);
     }
-    if(note!=='')
-        pData.push(["println",note]);
-    pData.push(["draw line"]);
     pData.push(["printlr","Subtotal:",order.ticket.cartSnapshot.subTotal.toFixed(2)]);
     pData.push(["printlr","Tax", order.ticket.salesTax.toFixed(2)]);
     pData.push(["printlr","Tip", order.ticket.tip.toFixed(2)]);
@@ -322,63 +327,6 @@ try{
 
 var fs = require('fs');
 var restName = "Baba Ghannouj Restaurant & Catering";
-async function checkServer()
-{
-    var d = new Date();
-    const todayStr = d.toLocaleDateString('en-US');
-    fs.appendFile('todayOrders.txt', '', function (err) {
-    if (err) throw err;
-        console.log('Saved!');
-    });
-    ordersPrinted = {};
-    fs.readFile('todayOrders.txt', 'utf8', function(err, data) {
-        if (err) throw err;
-        //console.log('readdata:',data);
-        var lines = data.split('\n');
-        if(lines.length==0 || lines[0].trim()!==todayStr){
-            fs.writeFile('todayOrders.txt', todayStr, function (err) {
-                if (err) throw err;
-                console.log('Cleared todayOrders.txt!');
-            });
-        }
-        else{
-            for(var i=1; i<lines.length; i++){
-                ordersPrinted[lines[i].trim()]=0;
-            }
-        }
-    });
-    var todayStart = new Date();
-    todayStart.setHours(0,0,0,0);
 
-    while(true){
-        db.collection("printerOrders")
-            .where("restName", "==", restName)
-            .where("orderTime", ">=", todayStart)
-        .get()
-        .then(function(querySnapshot) {
-            var appendData="";
-            querySnapshot.forEach(function(doc) {
-                // doc.data() is never undefined for query doc snapshots
-                if(!ordersPrinted.hasOwnProperty(doc.id)){
-                    printOrder(doc.id);
-                    console.log(doc.id);
-                    ordersPrinted[doc.id]=0;
-                    appendData+='\n'+doc.id;
-                }
-            });
-            if(appendData!==""){
-                fs.appendFile('todayOrders.txt', appendData, function (err) {
-                    if (err) throw err;
-                    console.log('Updated!');
-                });
-            }
-        })
-        .catch(function(error) {
-            console.log("Error getting documents: ", error);
-        });
-        await new Promise(r => setTimeout(r, 1000));
-    }
-}
 module.exports.print = print;
 module.exports.printOrder = printOrder;
-module.exports.checkServer = checkServer;
